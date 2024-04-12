d46b25a773d79ef35387e19adf08dd7d3d8be04d
AarynLu <hello@0x3f4.run>
2024-04-12 22:13:10 +0800

    feat: ✨ limit like interval

--- common/errs/err_code.go
+++ common/errs/err_code.go
@@ -21,6 +21,8 @@ const (
 	ErrWrongPassword
 
 	ErrRecordNotFound
+
+	ErrLikeIntervalTooShort
 )
 
 const (
--- service/like_service.go
+++ service/like_service.go
@@ -10,10 +10,18 @@ import (
 	"time"
 )
 
+var (
+	LatestLikeTime time.Time // 最近一次点赞的时间，防止短时间内多次点赞
+)
+
+const (
+	LikeInterval = 1 * time.Second // 点赞的时间间隔，1s
+)
+
 type LikeReq struct {
-	UserId       int `json:"userId"`
-	PromptId     int `json:"promptId"`
-	SellerUserId int `json:"sellerUserId"`
+	UserId   int `json:"userId"`
+	PromptId int `json:"promptId"`
+	SellerId int `json:"sellerId"`
 }
 
 func IsLike(c *gin.Context, userId, promptId int) (bool, *errs.Errs) {
@@ -26,6 +34,12 @@ func IsLike(c *gin.Context, userId, promptId int) (bool, *errs.Errs) {
 }
 
 func (r *LikeReq) Like(c *gin.Context) (err *errs.Errs) {
+	if LatestLikeTime.Add(LikeInterval).After(time.Now()) {
+		utils.Log().Warning(c.FullPath(), "点赞时间间隔过短")
+		return errs.NewErrs(errs.ErrLikeIntervalTooShort, errors.New("点赞时间间隔过短"))
+	}
+	LatestLikeTime = time.Now()
+
 	var count, addOrSubAmount int
 	if model.DB.Model(model.Likes{}).Where("user_id = ? AND prompt_id = ?", r.UserId, r.PromptId).Count(&count).Error != nil {
 		utils.Log().Error(c.FullPath(), "DB 查询是否点赞错误")
@@ -41,10 +55,10 @@ func (r *LikeReq) Like(c *gin.Context) (err *errs.Errs) {
 		addOrSubAmount = -1
 	} else { // 未点赞则点赞
 		likes := model.Likes{
-			UserId:       r.UserId,
-			PromptId:     r.PromptId,
-			SellerUserId: r.SellerUserId,
-			CreateTime:   time.Now(),
+			UserId:     r.UserId,
+			PromptId:   r.PromptId,
+			SellerId:   r.SellerId,
+			CreateTime: time.Now(),
 		}
 		if err := model.DB.Create(&likes).Error; err != nil {
 			utils.Log().Error(c.FullPath(), "点赞失败")
@@ -56,6 +70,10 @@ func (r *LikeReq) Like(c *gin.Context) (err *errs.Errs) {
 	if model.DB.Model(&model.Prompt{}).Where("id = ?", r.PromptId).UpdateColumn("like_amount", gorm.Expr("like_amount + ?", addOrSubAmount)).Error != nil {
 		utils.Log().Error(c.FullPath(), "更新点赞数失败")
 		return errs.NewErrs(errs.ErrDBError, errors.New("更新点赞数失败"))
+	}
+	if model.DB.Model(&model.Seller{}).Where("id = ?", r.SellerId).UpdateColumn("like_amount", gorm.Expr("like_amount + ?", addOrSubAmount)).Error != nil {
+		utils.Log().Error(c.FullPath(), "更新卖家获点赞数失败")
+		return errs.NewErrs(errs.ErrDBError, errors.New("更新卖家获点赞数失败"))
 	}
 	return nil
 
